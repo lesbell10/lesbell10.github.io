787a0132575778925152c8cbef06da3692a31773
Leslie <lesbellamy99@gmail.com>
2022-10-27 10:31:14 -0400

    yeh

---
+++ WebForm_GitHub/WebForm_GitHub/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebForm_GitHub.Startup))]
+namespace WebForm_GitHub
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
